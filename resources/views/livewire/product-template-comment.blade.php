8288e0758580393c9a7b4eb6723dda3e67f018d1
pemba17 <pemba.nuru59@gmail.com>
2023-01-03 15:47:19 +0545

    cc

--- resources/views/livewire/product-template-comment.blade.php
+++ resources/views/livewire/product-template-comment.blade.php
@@ -13,17 +13,19 @@
         @else
             @error('editComment') <div>{{$message}}</div> @enderror
             <form wire:keydown.enter="updateComment">
-                <textarea id="edit_comment" rows="6" wire:model.defer="editComment" class="w-1/2">
+                <textarea id="edit_comment" rows="6" wire:model.defer="editComment" class="w-full text-sm text-gray-900 border-gray-300">
                 </textarea>
             </form>
         @endif       
         <div class="flex items-center mt-4 space-x-4">
-            <button type="button" wire:click="setChild()"
-                class="flex items-center text-sm text-gray-500 hover:underline">
-                <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
-                Show {{$this->countReplies()}} Replies
-            </button>
-            <p>
+            @if(!$this->show_child)
+                <button type="button" wire:click="setChild()"
+                    class="flex items-center text-sm text-gray-500 hover:underline">
+                    <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+                    Show {{$this->countReplies()}} Replies
+                </button>
+            @endif    
+            <p class="text-gray-500">
                 {{$this->likes}} Likes
             </p>
             <button class="flex items-center text-sm text-gray-500 hover:underline" wire:click="$set('showCommentBox',true)">Reply</button>
@@ -36,23 +38,23 @@
                 <button class="flex items-center text-sm text-gray-500 hover:underline" wire:click="showEdit()">Edit</button>
                 <button class="flex items-center text-sm text-gray-500 hover:underline" wire:click="removeComment()">Delete</button>
             @endif    
-        </div>
-        @if($this->showCommentBox==true)
-            @error('postComment') <div>{{$message}}</div> @enderror
-            <form class="mt-3" wire:keydown.enter="addComment()">
-                <div class="py-2 px-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
-                    <label for="comment" class="sr-only">Your comment</label>
-                    <textarea id="comment" rows="6" 
-                        class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                        placeholder="Write a comment..." wire:model.defer="postComment"></textarea>
-                </div>
-            </form>
-        @endif     
+        </div>    
         @if(($comment->childs()->count()>0 && ($show_child)))
             {{--child--}}
             <div class="pl-5">
                 <livewire:plant-search.comment :product_id="$comment->product_id" :parent_id="$comment->id" key="{{ now() }}">      
             </div>
         @endif
+        @if($this->showCommentBox==true)
+            @error('postComment') <div>{{$message}}</div> @enderror
+            <form class="mt-3" wire:keydown.enter="addComment()">
+                <div class="py-2 px-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
+                    <label for="comment" class="sr-only">Your comment</label>
+                    <textarea id="comment" rows="6" 
+                        class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none"
+                        placeholder="Write a comment..." wire:model.defer="postComment"></textarea>
+                </div>
+            </form>
+        @endif 
     </div>
 </section>
