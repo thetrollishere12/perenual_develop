dcd3f8e9d3023d45ccd9a4ac4348859eada8c71d
pemba17 <pemba.nuru59@gmail.com>
2023-03-14 15:05:35 +0545

    fix null

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -5,15 +5,21 @@ Result
 <x-app-layout>
     <div class="w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         <h1 class="text-lg font-bold">Results</h1>
-        <div class="flex flex-col gap-2 mt-4">
-            <div>
-                <p>ID</p>
-                <span>{{$species->id}}</span>
+        @if($species)
+            <div class="flex flex-col gap-2 mt-4">
+                <div>
+                    <p>ID</p>
+                    <span>{{$species->id}}</span>
+                </div>
+                <div>
+                    <p>Common Namee</p>
+                    <span>{{$species->common_name}}</span>
+                </div>  
             </div>
+        @else
             <div>
-                <p>Common Namee</p>
-                <span>{{$species->common_name}}</span>
-            </div>  
-        </div>    
+                <p class="text-red-500">* No Species Result Found</p>
+            </div>
+        @endif        
     </div>
 </x-app-layout>
