a0f98bd1a00eecaa2cbf90e35abadf6783f06785
pemba17 <pemba.nuru59@gmail.com>
2023-03-17 14:45:42 +0545

    added description

--- resources/views/vendor/livewire-wizard/steps-footer.blade.php
+++ resources/views/vendor/livewire-wizard/steps-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="p-2 flex flex-row-reverse justify-between">
     @if($this->hasNextStep())
-        <x-button lg primary  right-icon="chevron-right" wire:click="goToNextStep" spinner="goToNextStep" :label="__('Next')"/>
+        <x-button lg primary  right-icon="chevron-right" wire:click="goToNextStep" spinner="goToNextStep" :label="__('Go To Next')" class="bg-green-500 text-white rounded-sm hover:bg-green-700"/>
     @else
         <x-button lg primary type="submit" spinner="submit" :label="__('Submit')"/>
     @endif
