b7ed2c464d7731b3c29389c4699c9e34b865fa9f
pemba17 <pemba.nuru59@gmail.com>
2023-03-21 09:46:05 +0545

    done

--- resources/views/steps/indoor.blade.php
+++ resources/views/steps/indoor.blade.php
@@ -1,9 +1,10 @@
-<div class="flex flex-col justify-center items-center">
-    <div>
+<div>
+    <div class="flex flex-col justify-center items-center">
         <x-jet-label for="indoor_type" value="{{ __('Choose Indoor') }}" />
-        <input type="radio" id="indoor" value="1" wire:model="state.indoor">
+        <input type="radio" id="indoor" value="1" wire:model="state.indoor" class="mt-2">
         <label for="indoor">Indoor</label><br>
-        <input type="radio" id="outdoor" value="0" wire:model="state.indoor">
+        <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower" class="w-50">
+        <input type="radio" id="outdoor" value="0" wire:model="state.indoor" class="mt-2">
         <label for="outdoor">Outdoor</label><br>
     </div>        
     
@@ -22,9 +23,6 @@
         <div class="space-y-2 w-1/2">
             <x-jet-label for="description" value="{{ __('Description') }}" />
             <textarea class="w-full rounded-md" rows="4" cols="80" wire:model="state.description"></textarea>
-        </div>  
-        <div class="space-y-2 flex justify-center mt-4"> 
-            <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower" class="w-50">
         </div>    
     </div>
 </div>
