95ec6d541d02ee1841ff714f2cbb7a701733f83f
pemba17 <pemba.nuru59@gmail.com>
2023-03-21 09:58:54 +0545

    designed changed

--- resources/views/steps/indoor.blade.php
+++ resources/views/steps/indoor.blade.php
@@ -1,11 +1,23 @@
 <div>
-    <div class="flex flex-col justify-center items-center">
-        <x-jet-label for="indoor_type" value="{{ __('Choose Indoor') }}" />
-        <input type="radio" id="indoor" value="1" wire:model="state.indoor" class="mt-2">
-        <label for="indoor">Indoor</label><br>
-        <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower" class="w-50">
-        <input type="radio" id="outdoor" value="0" wire:model="state.indoor" class="mt-2">
-        <label for="outdoor">Outdoor</label><br>
+    <div class="space-y-2">
+        <div class="flex justify-center">
+            <x-jet-label for="indoor_type" value="{{ __('Choose Indoor') }}" />
+        </div>
+        <div class="flex flex-col justify-center items-center">
+            <input type="radio" id="indoor" value="1" wire:model="state.indoor" class="mt-2">
+            <label for="indoor">Indoor</label><br>
+        </div>
+        <div class="flex justify-center">    
+            <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower" class="w-50">
+        </div> 
+        <div class="flex flex-col justify-center items-center">
+            <x-jet-label for="description" value="{{ __('Description') }}" />
+            <textarea class="rounded-md w-1/2" rows="4" cols="80" wire:model="state.description"></textarea>
+        </div> 
+        <div class="flex flex-col justify-center items-center">   
+            <input type="radio" id="outdoor" value="0" wire:model="state.indoor" class="mt-2">
+            <label for="outdoor">Outdoor</label><br>
+        </div>    
     </div>        
     
     @if($this->state['indoor']==="0") 
@@ -19,10 +31,10 @@
         </div>   
     @endif
 
-    <div class="mt-4 flex flex-col justify-center items-center">
+    {{-- <div class="mt-4 flex flex-col justify-center items-center">
         <div class="space-y-2 w-1/2">
             <x-jet-label for="description" value="{{ __('Description') }}" />
             <textarea class="w-full rounded-md" rows="4" cols="80" wire:model="state.description"></textarea>
         </div>    
-    </div>
+    </div> --}}
 </div>
