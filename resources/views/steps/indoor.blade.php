081bb3073a1e4ab93d92d350e9f9d5c51e350f65
pemba17 <pemba.nuru59@gmail.com>
2023-03-22 18:50:33 +0545

    design fixed indoor

--- resources/views/steps/indoor.blade.php
+++ resources/views/steps/indoor.blade.php
@@ -1,29 +1,29 @@
-<div>
-    <div class="space-y-2">
-        <div class="flex justify-center">
-            <x-jet-label for="indoor_type" value="{{ __('Choose Indoor') }}" />
-        </div>
-        <div class="flex flex-col justify-center items-center">
-            <input type="radio" id="indoor" value="1" wire:model="state.indoor" class="mt-2">
-            <label for="indoor">Indoor</label><br>
-        </div>
-        <div class="flex justify-center">    
-            <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower" class="w-50">
-        </div> 
-        <div class="flex flex-col justify-center items-center">
-            <x-jet-label for="description" value="{{ __('Description') }}" />
-            <textarea class="rounded-md w-1/2" rows="4" cols="80" wire:model="state.description"></textarea>
-        </div> 
-        <div class="flex flex-col justify-center items-center">   
-            <input type="radio" id="outdoor" value="0" wire:model="state.indoor" class="mt-2">
-            <label for="outdoor">Outdoor</label><br>
+<div class="container">
+    <div class="grid place-items-center">
+        <div class="w-1/2 space-y-2">  
+            <div class="flex items-center space-x-2">
+                <input type="radio" id="indoor" value="1" wire:model="state.indoor">
+                <label for="indoor">Indoor</label> 
+            </div>
+            <div>     
+                <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower">
+            </div>    
+            <div class="space-y-2">
+                <x-jet-label for="description" value="{{ __('Description') }}" />
+                <textarea class="rounded-md w-full" rows="4" cols="80" wire:model="state.description"></textarea>
+            </div>
+            <div class="flex items-center space-x-2">   
+                <input type="radio" id="outdoor" value="0" wire:model="state.indoor">
+                <label for="outdoor">Outdoor</label>
+            </div>  
         </div>    
-    </div>        
+    </div>   
+          
     
     @if($this->state['indoor']==="0") 
         <div class="mt-4 space-y-2">
             <x-jet-label for="location" value="{{ __('Choose Location') }}" />
-            <select wire:model="state.location" class="w-full rounded-md">
+            <select wire:model="state.location" class="w-1/2 rounded-md">
                 <option value="">Choose...</option>
                 <option value="nepal">Nepal</option>
                 <option value="china">China</option>
