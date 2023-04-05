48fb5af82153aa0e8c88c935440afda006296685
pemba17 <pemba.nuru59@gmail.com>
2023-04-05 10:54:13 +0545

    design changed

--- resources/views/steps/indoor.blade.php
+++ resources/views/steps/indoor.blade.php
@@ -1,9 +1,9 @@
 <div class="container">
     <div class="grid place-items-center">
         <div class="w-1/2 space-y-2">  
-            <div class="flex items-center space-x-2">
-                <input type="radio" id="indoor" value="1" wire:model="state.indoor">
-                <label for="indoor">Indoor</label> 
+            <div class="button w-24 bg-gray-500 text-white rounded-md cursor-pointer">
+                <input type="radio" id="indoor" value="1" wire:model="state.indoor" class="hidden">
+                <label for="indoor" class="p-2">Indoor</label> 
             </div>
             <div>     
                 <img src="https://www.floraqueen.com/blog/wp-content/uploads/2016/03/iStock_000059265530_Medium.jpg" alt="flower">
@@ -12,29 +12,31 @@
                 <x-jet-label for="description" value="{{ __('Description') }}" />
                 <textarea class="rounded-md w-full" rows="4" cols="80" wire:model="state.description"></textarea>
             </div>
-            <div class="flex items-center space-x-2">   
-                <input type="radio" id="outdoor" value="0" wire:model="state.indoor">
-                <label for="outdoor">Outdoor</label>
+            <div class="button w-24 bg-gray-500 text-white rounded-md cursor-pointer">   
+                <input type="radio" id="outdoor" value="0" wire:model="state.indoor" class="hidden">
+                <label for="outdoor" class="p-2">Outdoor</label>
             </div>  
+            @if($this->state['indoor']==="0") 
+                <div class="mt-4 space-y-2">
+                    <x-jet-label for="location" value="{{ __('Choose Location') }}" />
+                    <select wire:model="state.location" class="w-1/2 rounded-md">
+                        <option value="">Choose...</option>
+                        <option value="nepal">Nepal</option>
+                        <option value="china">China</option>
+                    </select>
+                </div>   
+            @endif
         </div>    
     </div>   
-          
-    
-    @if($this->state['indoor']==="0") 
-        <div class="mt-4 space-y-2">
-            <x-jet-label for="location" value="{{ __('Choose Location') }}" />
-            <select wire:model="state.location" class="w-1/2 rounded-md">
-                <option value="">Choose...</option>
-                <option value="nepal">Nepal</option>
-                <option value="china">China</option>
-            </select>
-        </div>   
-    @endif
 
-    {{-- <div class="mt-4 flex flex-col justify-center items-center">
-        <div class="space-y-2 w-1/2">
-            <x-jet-label for="description" value="{{ __('Description') }}" />
-            <textarea class="w-full rounded-md" rows="4" cols="80" wire:model="state.description"></textarea>
-        </div>    
-    </div> --}}
+    <style>
+        .button input[type="radio"]:checked + label {
+            background: #20b8be;
+            border-radius: 6px;
+            width: 100px;
+            height: 40px;
+            padding: 4px;
+            color: white;
+        }
+    </style>
 </div>
--- resources/views/steps/watering.blade.php
+++ resources/views/steps/watering.blade.php
@@ -1,11 +1,28 @@
 <div class="flex flex-col justify-center items-center">
-    <div>
+    <div class="space-y-4">
         <x-jet-label for="wateringType" value="{{ __('Choose Place') }}" />
-        <input type="radio" id="frequent" value="Frequent" wire:model="state.watering">
-        <label for="frequent">Frequent</label><br>
-        <input type="radio" id="average" value="Average" wire:model="state.watering">
-        <label for="average">Average</label><br>
-        <input type="radio" id="minimum" value="Minimum" wire:model="state.watering">
-        <label for="minimum">Minimum</label><br>
-    </div>    
+        <div class="button w-24 bg-gray-500 text-white rounded-md cursor-pointer">
+            <input type="radio" id="frequent" value="Frequent" wire:model="state.watering" class="hidden">
+            <label for="frequent" class="p-2">Frequent</label><br>
+        </div>
+        <div class="button w-24 bg-gray-500 text-white rounded-md cursor-pointer">    
+            <input type="radio" id="average" value="Average" wire:model="state.watering" class="hidden">
+            <label for="average"  class="p-2">Average</label><br>
+        </div>
+        <div class="button w-24 bg-gray-500 text-white rounded-md cursor-pointer">   
+            <input type="radio" id="minimum" value="Minimum" wire:model="state.watering" class="hidden">
+            <label for="minimum"  class="p-2">Minimum</label><br>
+        </div>    
+    </div>
+    
+    <style>
+        .button input[type="radio"]:checked + label {
+            background: #20b8be;
+            border-radius: 6px;
+            width: 100px;
+            height: 40px;
+            padding: 4px;
+            color: white;
+        }
+    </style>    
 </div>
